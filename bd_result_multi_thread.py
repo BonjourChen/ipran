14d86bd8027e7ff1ae5db8f5d67b622f86148fb6
wb <346517772@qq.com>
2016-02-01 23:37:56 +0800

    commit

--- bd_result_multi_thread.py
+++ bd_result_multi_thread.py
@@ -5,12 +5,36 @@ import os
 import sys
 import re
 import threading
+import time
+import queue
 
 varPath = os.path.dirname(__file__)
 if varPath not in sys.path:
 	sys.path.insert(0, varPath)
 
 from IPRANLibs import *
+
+class BD_Result_Thread(threading.Thread):
+	"""docstring for BD_Result_Thread"""
+	def __init__(self,lock,threadName):
+		super(BD_Result_Thread, self).__init__(name = threadName)
+		self.lock = lock
+
+	def run(self):
+		global myQueue
+		global resultDict
+		#listResult = threading.local()
+		while True:
+			try:
+				hostIp = myQueue.get(block = False)
+				listResult = bd_result(hostIp)
+				print (self.name, hostIp)
+				self.lock.acquire()
+				resultDict[hostIp] = listResult
+				self.lock.release()
+			except queue.Empty:
+				print(self.name + ' finish at %s' %time.ctime())
+				break
 
 def getHosename(child):
 	loginprompt = '[#>]'
@@ -25,18 +49,8 @@ def getHosename(child):
 		hostname = str(child.before, encoding = 'utf-8')
 	return hostname
 
-
-def HW_B(child):
-	# loginprompt = '[#>]'
-	# child.sendline('dis isis peer')
-	# child.expect('dis isis peer')
-	# child.sendline(' ')
-	# child.expect(loginprompt)
-	# result_isis = child.before
-
+def HW_B(child, listResult):
 	result_isis = cmd.cmd_show(child,'dis isis peer','More','>')
-
-	global listResult
 	listBPeer = re.findall(r'[a-zA-Z0-9\.-]+-B-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listDPeer = re.findall(r'[a-zA-Z0-9\.-]+-D-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listMPeer = re.findall(r'[a-zA-Z0-9\.-]+-M-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
@@ -80,12 +94,10 @@ def HW_B(child):
 			listResult.append('Only uT')
 		else:
 			listResult.append('unknown')
-
 	return listResult
 	
-def ZTE_B(child):
+def ZTE_B(child, listResult):
 	result_isis = cmd.cmd_show(child,'show isis adjacency','More','#')
-	global listResult
 	listBPeer = re.findall(r'[a-zA-Z0-9\.-]+-B-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listDPeer = re.findall(r'[a-zA-Z0-9\.-]+-D-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listMPeer = re.findall(r'[a-zA-Z0-9\.-]+-M-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
@@ -131,9 +143,8 @@ def ZTE_B(child):
 			listResult.append('unknown')
 	return listResult
 
-def Fiber_B(child):
+def Fiber_B(child, listResult):
 	result_isis = cmd.cmd_show(child,'show clns is-neighbors','More','#')
-	global listResult
 	listBPeer = re.findall(r'[a-zA-Z0-9\.-]+-B-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listDPeer = re.findall(r'[a-zA-Z0-9\.-]+-D-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
 	listMPeer = re.findall(r'[a-zA-Z0-9\.-]+-M-[a-zA-Z0-9\.-]*(?=\s+)', result_isis)
@@ -179,8 +190,7 @@ def Fiber_B(child):
 			listResult.append('unknown')
 	return listResult
 
-def bd_result(ip,resultDict):
-	global listResult
+def bd_result(ip):
 	listResult = []
 	try:
 		c = connect.Connector('gdcwb','123456Qw!2')
@@ -191,40 +201,35 @@ def bd_result(ip,resultDict):
 			listResult.append('Local')
 		elif loginMode == 'Failed':
 			listResult = ['Failed','','','','','','','']
-			resultDict[ip] = listResult
 		elif loginMode == 'No':
 			listResult = ['No','','','','','','','']
-			resultDict[ip] = listResult
 
 		if loginMode == '3A' or loginMode == 'Local':
 			hostname = getHosename(child)
 			listResult.append(hostname)
-			print(hostname)
+			#print(hostname)
 			if re.search(r'910|950',hostname):
 				listResult.append('HW-A')
 			elif re.search(r'CX600',hostname):
 				listResult.append('HW-B/D')
-				listResult = HW_B(child)
+				listResult = HW_B(child, listResult)
 			elif re.search(r'6130|6150|6220',hostname):
 				listResult.append('ZTE-A')
 			elif re.search(r'9000',hostname):
 				listResult.append('ZTE-B/D')
-				listResult = ZTE_B(child)
+				listResult = ZTE_B(child, listResult)
 			elif re.search(r'R835E|R820',hostname):
 				listResult.append('Fiber-A')
 			elif re.search(r'R8000',hostname):
 				listResult.append('Fiber-B/D')
-				listResult = Fiber_B(child)
+				listResult = Fiber_B(child,listResult)
 			else:
 				listResult.append('Unknown')
-			resultDict[ip] = listResult
-			print(resultDict[ip])
 		elif loginMode == 'Failed' or loginMode == 'No':
 			print(ip + ' Login Failed')
-			print(resultDict[ip])
+		return listResult
 	except Exception as e:
-			print(e)
-
+			print(ip + str(e))
 
 try:
 	loginIp = []
@@ -235,13 +240,21 @@ try:
 	result = cur.fetchall()
 	for row in result:
 		loginIp.append(row[1])
+
 	resultDict = {}
 
-	for index, ip in enumerate(loginIp):
-		#bd_result(ip,resultDict)
-		t = threading.Thread(target = bd_result, args = (ip,resultDict,))
-		t.start()
+	myQueue = queue.Queue()
+	for ip in loginIp:
+		myQueue.put(ip)
 
+	lock = threading.Lock()
+	threads = []
+	for i in range(20):
+		threads.append(BD_Result_Thread(lock,"thread-" + str(i)))
+	for t in threads:
+		t.start()
+	for t in threads:
+		t.join()
 
 except pymysql.Error as e:
 	print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
